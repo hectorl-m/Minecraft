de45ffcf78533a9cf69560b3ded9133f9a1e852b
hectorl-m <hectorlm203@gmail.com>
2025-01-10 16:28:53 +0100

    curiosidades
    muestra una curiosidad aleatoria al pulsar el botón de mas curiosidades

--- script.js
+++ script.js
@@ -2,9 +2,23 @@
 const infoButton = document.getElementById("info-button");
 const infoDialog = document.getElementById("info-dialog");
 const closeDialog = document.getElementById("close-dialog");
+const dialogText = infoDialog.querySelector("p");
 
-// Mostrar el diálogo
+// Lista de curiosidades
+const curiosidades = [
+    "La unidad de medida en Minecraft se llama 'bloque', equivalente a un metro en el mundo real.",
+    "El creador de Minecraft, Markus Persson, también conocido como Notch, desarrolló la primera versión del juego en solo una semana.",
+    "Los Creepers se originaron debido a un error al programar a los cerdos.",
+    "El bioma más raro del juego es el " + "'Mushroom Fields' (Campos de setas).",
+    "Los pandas en Minecraft pueden estornudar y hacer que otros pandas reaccionen.",
+    "Minecraft es el videojuego más vendido de todos los tiempos.",
+    "Existen libros y estudios académicos sobre el uso de Minecraft como herramienta educativa."
+];
+
+// Mostrar un dato aleatorio en el diálogo
 infoButton.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * curiosidades.length);
+    dialogText.textContent = curiosidades[randomIndex];
     infoDialog.showModal();
 });
 
